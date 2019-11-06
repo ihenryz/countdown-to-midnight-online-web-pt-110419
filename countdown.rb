e8a95d98aa6f7c7656084da136565494d3870f33
Henry Phan <h.phan06@gmail.com>
2019-11-06 06:46:27 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,11 +10,13 @@ def countdown(count)
 end
 
 def countdown_with_sleep(x)
-  count = 10
-  while count > 0 do
-    puts "#{count} SECONDS(S)!"
-    sleep 1
-    count -= 1
-  end
-  "HAPPY NEW YEAR!"
+  
+  countdown(count) + sleep 1
+  # count = 10
+  # while count > 0 do
+  #   puts "#{count} SECONDS(S)!"
+  #   sleep 1
+  #   count -= 1
+  # end
+  # "HAPPY NEW YEAR!"
 end
